fe9b2ff99e74b0216822eb276bc52962bf4db636
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-02-24 10:51:10 -0500

    adding views folder

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,20 @@
 const connection = require('../config/connection');
 
 const orm = {
-    selectAll: function(table, cb) {
-        connection.query("SELECT * FROM ??", [table], function(err, results) {
+    selectAll: (table, cb) => {
+        connection.query("SELECT * FROM ??", [table], (err, results) => {
             if (err) throw err;
             cb(results);
         })
     },
-    insertOne: function(table, columns, values, cb) {
-        connection.query("INSERT INTO ?? (??) VALUES (?);", [table, columns, values], function(err, results) {
+    insertOne: (table, columns, values, cb) => {
+        connection.query("INSERT INTO ?? (??) VALUES (?);", [table, columns, values], (err, results) => {
             if (err) throw err;
             cb(results);
         })
     },
-    updateOne: function(table, setColumn, setVal, conditionalCol, conditionalVal, cb) {
-        connection.query("UPDATE ?? SET ?? = ? WHERE ?? = ?", [table, setColumn, setVal, conditionalCol, conditionalVal], function(err, results) {
+    updateOne: (table, setColumn, setVal, conditionalCol, conditionalVal, cb) => {
+        connection.query("UPDATE ?? SET ?? = ? WHERE ?? = ?", [table, setColumn, setVal, conditionalCol, conditionalVal], (err, results) => {
             if (err) throw err;
             cb(results);
         })
