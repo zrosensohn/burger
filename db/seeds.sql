06232312a6b1216d1b91887e6821a4cb9ffb33d9
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-02-24 09:10:20 -0500

    pushing db and config file structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Rodeo Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Flamethrower');
+INSERT INTO burgers (burger_name) VALUES ('Blue Bayou');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese', true);
