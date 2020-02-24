f9a3151dcf10d42b084587a1b428d7b5644231ea
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-02-24 09:08:33 -0500

    finished and tested orm and db files

---
+++ config/orm.js
@@ -0,0 +1,24 @@
+const connection = require('../config/connection');
+
+const orm = {
+    selectAll: function(table, cb) {
+        connection.query("SELECT * FROM ??", [table], function(err, results) {
+            if (err) throw err;
+            cb(results);
+        })
+    },
+    insertOne: function(table, columns, values, cb) {
+        connection.query("INSERT INTO ?? (??) VALUES (?);", [table, columns, values], function(err, results) {
+            if (err) throw err;
+            cb(results);
+        })
+    },
+    updateOne: function(table, setColumn, setVal, conditionalCol, conditionalVal, cb) {
+        connection.query("UPDATE ?? SET ?? = ? WHERE ?? = ?", [table, setColumn, setVal, conditionalCol, conditionalVal], function(err, results) {
+            if (err) throw err;
+            cb(results);
+        })
+    }
+}
+
+module.exports = orm;
