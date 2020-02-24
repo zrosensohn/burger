b29d58ae855ca24b927e69beb2890e3469a241b3
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-02-24 10:09:04 -0500

    model file finished

--- models/burger.js
+++ models/burger.js
@@ -2,16 +2,20 @@ const orm = require("../config/orm");
 
 const burger = {
     selectStar: function (cb) {
-        orm.selectAll("burgers", function(results){
+        orm.selectAll("burgers", (results) => {
             cb(results);
+        })
+    },
+    insert: function(name, devoured, cb) {
+        orm.insertOne("burgers", ["burger_name", "devoured"], [name, devoured], (results) => {
+            cb(results)
+        })
+    },
+    update: function(devoured, conditionalCol, conditionalVal, cb) {
+        orm.updateOne("burgers", "devoured", devoured, conditionalCol, conditionalVal, (results) => {
+            cb(results)
         })
     }
 }
 
-orm.selectAll("burgers", function(results) {
-    console.log(results);
-})
-
-// burger.selectStar(results => {
-//     console.log(results);
-// })
+module.exports = burger;
