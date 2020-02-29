cc25a97fe20baacf0a8568b75f386f0fd3b7086e
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-02-29 07:12:47 -0500

    jaws db stuff

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: process.env.DBHOST,
-  port:  process.env.DBPORT,
-  user:  process.env.DBUSER,
-  password:  process.env.DBPASSWORD,
-  database:  process.env.DBNAME
-});
+var connection;
 
 // Make connection.
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBNAME
+  });
+}
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
